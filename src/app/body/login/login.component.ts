2f60ad64b2a969fb81d7428cc449e293d119ace4
Nasir M <nasir.m23@hotmail.com>
2018-02-22 23:34:23 -0500

    Implement auth front-end

--- src/app/body/login/login.component.ts
+++ src/app/body/login/login.component.ts
@@ -7,11 +7,21 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
   hidePassword = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getUsernameErrorMessage() {
+    return this.username.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' : '';
   }
 
 }
--- src/app/body/register/register.component.ts
+++ src/app/body/register/register.component.ts
@@ -9,15 +9,33 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
+  username = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]);
+  password = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]);
+  confirmPassword = new FormControl('', [Validators.required]);
   hidePassword = true;
 
-  getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
-            '';
+  constructor() {
   }
 
-  constructor() { }
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+
+  getUsernameErrorMessage() {
+    return this.username.hasError('minlength') ? 'Username be greater than 2 characters' :
+      this.username.hasError('maxlength') ? 'Username be less than 21 characters' :
+        this.username.hasError('required') ? 'You must enter a value' :
+          '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('minlength') ? 'Password be greater than 4 characters' :
+      this.password.hasError('maxlength') ? 'Password must be less than 21 characters' :
+        this.password.hasError('required') ? 'You must enter a value' :
+          '';
+  }
 
   ngOnInit() {
   }
