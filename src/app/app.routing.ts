9f004caff407a5eac2041ab334f9d358a9f59960
Nasir Mohammad <nasir@sourceapp.io>
2018-03-09 15:13:03 -0500

    fix style of containers

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,12 @@ import { Route } from '@angular/compiler/src/core';
 import { LoggedInGuard } from './body/logged-in.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', loadChildren: './body/home/home.module#HomeModule' },
   { path: 'about', loadChildren: './body/about/about.module#AboutModule' },
   { path: 'login', loadChildren: './body/login/login.module#LoginModule', canActivate: [ LoggedInGuard ] },
   { path: 'register', loadChildren: './body/register/register.module#RegisterModule', canActivate: [ LoggedInGuard ] },
-  { path: 'talent', loadChildren: './body/talent-calculator/talent-calculator.module#TalentCalculatorModule' }
+  { path: 'talent/:classId', loadChildren: './body/talent-calculator/talent-calculator.module#TalentCalculatorModule' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
