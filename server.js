fd2b5ef6b23f7e0eb052af215036006e870a79d7
c3mohamn <nasir.m23@hotmail.com>
2019-07-10 14:49:44 -0400

    Update server and package

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express'),
-      bodyParser = require('body-parser'),
-      path = require('path'),
-      http = require('http'),
-      app = express(),
-      expressValidator = require('express-validator');
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  expressValidator = require('express-validator');
 
-// API files
+const app = express();
+const port = process.env.PORT || 3000;
 const api = require('./server/routes/api');
 
 // Parsers
@@ -16,22 +15,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Express validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+  })
+);
 
 // API location
 app.use('/api', api);
@@ -41,12 +42,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log(`Running on localhost: ${port}.`));
+app.listen(port, () => console.log(`Running on localhost: ${port}.`));
 
 module.exports = app;
